214f9d27201e2d6474aa3502c1923d9cf1ed1bff
Sebastian S. Cocioba <sebastian@binomicalabs.org>
2022-01-24 12:47:07 -0500

    added minigel files

---
+++ KiCAD Part Library/TSL25721FN.mod
@@ -0,0 +1,62 @@
+PCBNEW-LibModule-V1  2022-01-18 04:47:21
+# encoding utf-8
+Units mm
+$INDEX
+FN
+$EndINDEX
+$MODULE FN
+Po 0 0 0 15 61e64659 00000000 ~~
+Li FN
+Cd FN
+Kw Undefined or Miscellaneous
+Sc 0
+At SMD
+AR 
+Op 0 0 0
+T0 -0.40778 -0.00184 1.27 1.27 0 0.254 N V 21 N "U**"
+T1 -0.40778 -0.00184 1.27 1.27 0 0.254 N I 21 N "FN"
+DS -1 -1 1 -1 0.254 24
+DS 1 -1 1 1 0.254 24
+DS 1 1 -1 1 0.254 24
+DS -1 1 -1 -1 0.254 24
+DS -1 -1 1 -1 0.254 21
+DS 1 1 -1 1 0.254 21
+DC -1.781 -0.687 -1.852 -0.687 0.254 21
+$PAD
+Po -0.8 -0.65
+Sh "1" R 0.4 1.3 0 0 900
+At SMD N 00888000
+Ne 0 ""
+$EndPAD
+$PAD
+Po -0.8 0
+Sh "2" R 0.4 1.3 0 0 900
+At SMD N 00888000
+Ne 0 ""
+$EndPAD
+$PAD
+Po -0.8 0.65
+Sh "3" R 0.4 1.3 0 0 900
+At SMD N 00888000
+Ne 0 ""
+$EndPAD
+$PAD
+Po 0.8 0.65
+Sh "4" R 0.4 1.3 0 0 900
+At SMD N 00888000
+Ne 0 ""
+$EndPAD
+$PAD
+Po 0.8 0
+Sh "5" R 0.4 1.3 0 0 900
+At SMD N 00888000
+Ne 0 ""
+$EndPAD
+$PAD
+Po 0.8 -0.65
+Sh "6" R 0.4 1.3 0 0 900
+At SMD N 00888000
+Ne 0 ""
+$EndPAD
+$EndMODULE FN
+$EndLIBRARY
---
+++ MiniSpec 2/Design Files/SensorStick-TSL2572/SensorStick-TSL2572-Test-Code/SensorStick-OPT3002-Test-Code.ino
@@ -0,0 +1,114 @@
+#include <Wire.h>
+#include "ClosedCube_OPT3002.h"
+
+ClosedCube_OPT3002 opt3002;
+
+#define OPT3002_ADDRESS 0x44    //originially listed as 0x45 but it's F$@%KING NOT 0x45!!!
+
+void setup()
+{
+	Serial.begin(115200);
+	//Serial.println("ClosedCube OPT3002 Arduino Test");
+
+	opt3002.begin(OPT3002_ADDRESS);
+	//Serial.print("OPT3002 Manufacturer ID:");
+	//Serial.print(opt3002.readManufacturerID());
+	//Serial.print(" Device ID:");
+	//Serial.println(opt3002.readDeviceID());
+
+	configureSensor();
+  
+	//printResult("", opt3002.readHighLimit());
+	//printResult("", opt3002.readLowLimit());
+	//Serial.println("----");
+  OPT3002 result = opt3002.readResult();
+}
+
+
+
+void loop()
+{
+	OPT3002 result = opt3002.readResult();
+	printResult("OPT3002", result);
+	delay(1000);
+}
+
+void printResult(String text, OPT3002 result) 
+{
+	if (result.error == NO_ERROR) 
+	{
+		Serial.println(result.lux);
+		//Serial.println(" nW/cm2");
+	}
+	
+	else 
+	{
+		printError(text, result.error);
+	}
+}
+
+
+
+void printError(String text, OPT3002_ErrorCode error) 
+{
+	Serial.print(text);
+	Serial.print(": [ERROR] Code #");
+	Serial.println(error);
+}
+
+
+
+void configureSensor() 
+{
+	OPT3002_Config newConfig;
+
+	newConfig.RangeNumber = B0111;    //B0111 enables up to 628,992 nW/cm2, use B1000 for the 10mm cuvette since light is closer to sensor
+	newConfig.ConvertionTime = B1;
+	newConfig.Latch = B1;
+	newConfig.ModeOfConversionOperation = B10;
+
+	OPT3002_ErrorCode errorConfig = opt3002.writeConfig(newConfig);
+	if (errorConfig != NO_ERROR)
+		printError("OPT3002 configuration", errorConfig);
+/*	else {
+		OPT3002_Config sensorConfig = opt3002.readConfig();
+		Serial.println("OPT3002 Current Config:");
+		Serial.println("------------------------------");
+
+		Serial.print("Conversion ready (R):");
+		Serial.println(sensorConfig.ConversionReady, HEX);
+
+		Serial.print("Conversion time (R/W):");
+		Serial.println(sensorConfig.ConvertionTime, HEX);
+
+		Serial.print("Fault count field (R/W):");
+		Serial.println(sensorConfig.FaultCount, HEX);
+
+		Serial.print("Flag high field (R-only):");
+		Serial.println(sensorConfig.FlagHigh, HEX);
+
+		Serial.print("Flag low field (R-only):");
+		Serial.println(sensorConfig.FlagLow, HEX);
+
+		Serial.print("Latch field (R/W):");
+		Serial.println(sensorConfig.Latch, HEX);
+
+		Serial.print("Mask exponent field (R/W):");
+		Serial.println(sensorConfig.MaskExponent, HEX);
+
+		Serial.print("Mode of conversion operation (R/W):");
+		Serial.println(sensorConfig.ModeOfConversionOperation, HEX);
+
+		Serial.print("Polarity field (R/W):");
+		Serial.println(sensorConfig.Polarity, HEX);
+
+		Serial.print("Overflow flag (R-only):");
+		Serial.println(sensorConfig.OverflowFlag, HEX);
+
+		Serial.print("Range number (R/W):");
+		Serial.println(sensorConfig.RangeNumber, HEX);
+
+		Serial.println("------------------------------");
+	} */
+
+}
